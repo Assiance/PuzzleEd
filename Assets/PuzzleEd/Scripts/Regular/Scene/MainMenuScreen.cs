6839b59e7fb68ab0c02e88380695226a4900ec79
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2015-01-28 21:07:53 -0500

    pushed art and added scenes

---
+++ Assets/PuzzleEd/Scripts/Regular/Scene/MainMenuScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Assets.PuzzleEd.Scripts.Regular.General;
+
+namespace Assets.PuzzleEd.Scripts.Regular.Scene
+{
+    public class MainMenuScreen : ESMonoBehaviour
+    {
+        public void PlayGame()
+        {
+            Application.LoadLevel("PlayScene");
+        }
+    }
+}
